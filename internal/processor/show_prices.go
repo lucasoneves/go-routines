578da238cd5ae62ec72ba07c2e44ffdbea98015e
lucasoneves <casluhc@gmail.com>
2025-05-28 17:59:06 -0300

    Formatando a data

--- internal/processor/show_prices.go
+++ internal/processor/show_prices.go
@@ -13,7 +13,7 @@ func ShowPriceAverage(priceChannel <-chan models.ProductDetail, done chan<- bool
 		countPrices++
 		averagePrice := totalPrice / float64(countPrices)
 
-		fmt.Printf("Preço de %s | R$ %.2f - Preço médio: R$ %.2f \n", price.StorageName, price.Value, averagePrice)
+		fmt.Printf("Data: %s | Preço de %s | R$ %.2f - Preço médio: R$ %.2f \n", price.TimeStamp.Format("02/01/2006 15:04"), price.StorageName, price.Value, averagePrice)
 	}
 	done <- true
 }
